a0c2e5af693ab0f4b0673c24ba2b9d98e9bec9ab
syp95 <sy95_055@naver.com>
2023-05-24 22:12:03 +0900

    feat: mixins

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status
+from rest_framework import status, generics, mixins
 from .models import Book
 from .serializers import BookSerializer
 
@@ -11,14 +11,22 @@ def HelloAPI(request):
     return Response('hello world')
 
 
-class BookAPI(APIView):
-    def get(self,request):
-        books = Book.objects.all()
-        serializer = BookSerializer(books, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    def post(self, request):
-        serializer = BookSerializer(data= request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# class BookAPI(APIView):
+#     def get(self,request):
+#         books = Book.objects.all()
+#         serializer = BookSerializer(books, many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#     def post(self, request):
+#         serializer = BookSerializer(data= request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BookAPI(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    def get(self,request,*args,**kwargs):
+        return self.list(request,*args,**kwargs)
+    def post(self,request,*args,**kwargs):
+        return self.create(request,*args,**kwargs)
